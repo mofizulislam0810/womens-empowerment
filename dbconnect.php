82d4ed4796cc8876b923bfda3260454dec02a8fa
mofizulislam0810 <mofizul.islam0810@gmail.com>
2022-07-27 10:44:43 +0600

    27-07-2022

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost", "root", "0810", "finalproject") or
+$con = mysqli_connect("sql108.epizy.com", "epiz_32254426", "Prpg93QTLlUPUwl", "epiz_32254426_womenpower") or
 die("Error " . mysqli_error($con));
 ?>
